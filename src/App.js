d5c50abf2aaff4e9c97ff6ee85ce24a9c4bf1a87
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-04 12:04:11 -0800

    add login with redirect to users

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import ReactDOM from 'react-dom';
 import { Router, Route, Link } from 'react-router';
 import {RaisedButton} from 'material-ui';
 require('./main.scss');
+
 export default class App extends React.Component {
+
+  onSubmit() {
+    //mock user for now
+    window.localStorage._amtoken =  'secrettoken';
+
+    //on success
+    if (window.localStorage._amtoken) {
+     // reroute immediately and dont' save
+      window.location.href = window.location.origin + '/users';
+    }
+
+  }
   render() {
     return (
       <div style={{
