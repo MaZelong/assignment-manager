053133e1bb819f07cba491d45b9e47ef60b60e39
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-04 11:51:47 -0800

    refactor to upgrade to rr 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Link } from 'react-router'
+import { Router, Route, Link } from 'react-router';
+import {RaisedButton} from 'material-ui';
 require('./main.scss');
 export default class App extends React.Component {
   render() {
     return (
-       <div style={{height: "100%"}}>
-       	Welcome!
-       </div>
+      <div style={{
+        width: "100%",
+        height: "100%",
+        display: 'flex',
+        flex:1,
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center',
+        margin: "auto"
+        }}
+      >
+        <RaisedButton
+          label="Login with Google"
+          onClick={this.onSubmit}
+          style={{
+            width: "30%"
+          }}
+          secondary="true"
+        />
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom'
-import { createHistory, useBasename } from 'history'
-import { Router, Route, Link } from 'react-router'
+import {Router, Route, Link } from 'react-router'
 import App from './App'
+import { createHistory, useBasename } from 'history'
 const history = useBasename(createHistory)({
-    basename: '/'
+  basename: '/'
 });
 render((
     <Router history={history}>
